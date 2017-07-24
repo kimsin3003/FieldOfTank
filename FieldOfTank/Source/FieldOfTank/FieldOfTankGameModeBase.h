ccdafe08c33a52f3358ecb27700b88803c225abf
home <kimsin3003@naver.com>
2017-07-24 19:49:06 +0900

    for next

---
+++ FieldOfTank/Source/FieldOfTank/FieldOfTankGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "FieldOfTankGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FIELDOFTANK_API AFieldOfTankGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
